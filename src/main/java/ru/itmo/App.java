17bb10db86dbed9e48e8af283176abbdac74c7a9
Andryss <andr.krivosheev@gmail.com>
2025-09-11 22:48:26 +0300

    Add checkstyle

--- src/main/java/ru/itmo/App.java
+++ src/main/java/ru/itmo/App.java
@@ -1,10 +1,9 @@
 package ru.itmo;
 
-/**
- * Hello world!
- *
- */
 public class App {
+    /**
+     * Входная точка в приложение
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }

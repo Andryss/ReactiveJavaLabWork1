bb6ebdcc55b1a7c9976436d7f66a87670ee7e771
Andryss <andr.krivosheev@gmail.com>
2025-12-05 16:54:26 +0300

    Add ping controller test

---
+++ src/test/java/ru/itmo/spaceships/controller/PingControllerTest.java
@@ -0,0 +1,41 @@
+package ru.itmo.spaceships.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PingControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    WebTestClient webClient;
+
+    @Test
+    void pingTest() {
+        String url = "http://localhost:" + port + "/ping";
+        String response = restTemplate.getForObject(url, String.class);
+
+        assertEquals("pong", response);
+    }
+
+    @Test
+    void reactivePingTest() {
+        webClient.get().uri("/ping")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().consumeWith(result ->
+                        assertEquals("pong", new String(result.getResponseBody()))
+                );
+    }
+}
